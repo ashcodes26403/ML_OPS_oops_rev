1fd0e40eebcfad5e3d37cfe875d0b968dad2d076
Ashmit <ashmitsinha.26403@gmail.com>
2025-06-19 05:18:58 +0530

    Project basic structure ready

---
+++ oops_proj.py
@@ -0,0 +1,22 @@
+class chatbook:
+    def __init__(self):
+        self.username = ''
+        self.password = ''
+        self.loggedin = False
+        self.menu()
+
+    def menu(self):
+        user_input = input("Welcome to chatbook, press 1 to 5, ykw to do")
+        if user_input == "1":
+            pass
+        elif user_input == "2":
+            pass
+        elif user_input == "3":
+            pass
+        elif user_input == "4":
+            pass
+        else:
+            exit()
+
+obj = chatbook()
+
